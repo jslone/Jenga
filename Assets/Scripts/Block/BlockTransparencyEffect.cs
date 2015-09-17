20eb8b5c0cc5f519fb2117fafb898f43bf7fd6db
unknown <sotolongomiguel@gmail.com>
2015-09-17 00:26:15 -0400

    Make nearby blocks transparent, - added block rotation using Q and E

---
+++ Assets/Scripts/Block/BlockTransparencyEffect.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockTransparencyEffect : MonoBehaviour {
+
+    static LayerMask clickableLayer;
+    public float distance = 1f;
+
+    // currently held piece
+    // used by blocks to determine fade effect
+    Rigidbody heldPiece = null;
+
+    // Use this for initialization
+    void Start () {
+        clickableLayer = LayerMask.GetMask("Blocks");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // check for selecting a piece
+        if (Input.GetMouseButtonDown((int)MouseButton.Left))
+        {
+            Ray mouseClickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(mouseClickRay, out hitInfo, float.PositiveInfinity, clickableLayer))
+            {
+                heldPiece = hitInfo.collider.GetComponent<Rigidbody>();
+            }
+        }
+
+        // check for letting go of a piece
+        if (Input.GetMouseButtonUp((int)MouseButton.Left) && heldPiece != null)
+        {
+            heldPiece = null;
+        }
+
+        if(heldPiece != null) {
+            FadeClosest();
+        }
+    }
+
+    void FadeClosest() {
+        GameObject[] tower = GameObject.FindGameObjectsWithTag("Block");
+        Vector3 pos = heldPiece.transform.position;
+        Rigidbody rb;
+        Renderer curRenderer;
+        Color curColor;
+        float curDistance = 0;
+
+        foreach(GameObject block in tower) {
+            rb = block.GetComponent<Rigidbody>();
+
+            curDistance = Vector3.Distance(pos, rb.transform.position);
+            curRenderer = block.GetComponentInChildren<Renderer>();
+            curColor = curRenderer.material.color;
+            for(int i = 0; i < curRenderer.materials.Length; i++) {
+                 if (rb != heldPiece && curDistance < distance) {
+                     curRenderer.materials[i].shader = Shader.Find("Transparent/Diffuse");
+                     curRenderer.materials[i].color = new Color(curColor.r, curColor.g, curColor.b, 0.2f);
+                 } else {
+                     curRenderer.materials[i].shader = Shader.Find("Standard");
+                     curRenderer.materials[i].color = new Color(curColor.r, curColor.g, curColor.b, 1.0f);
+                 }
+            }    
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Input/KeyboardControls.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardControls : MonoBehaviour {
+
+    private GameObject currentBlock;
+    private LayerMask clickableLayer;
+
+    public bool enableWASD;
+    public bool enableQE;
+
+
+	// Use this for initialization
+	void Start () {
+        clickableLayer = LayerMask.GetMask("Blocks");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown((int)MouseButton.Left)) {
+            Ray mouseClickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(mouseClickRay, out hitInfo, float.PositiveInfinity, clickableLayer)) {
+                currentBlock = hitInfo.transform.gameObject;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (currentBlock != null)
+        {
+            Rigidbody rb = currentBlock.GetComponent<Rigidbody>();
+            float moveHorizontal = Input.GetAxis("kHorizontal");
+            float moveVertical = Input.GetAxis("kVertical");
+            float moveUp = Input.GetAxis("kUp");
+            float rotation = Input.GetAxis("kRotation");
+
+            if(enableQE) {
+                Vector3 rotate = new Vector3(0, rotation, 0);
+                currentBlock.transform.Rotate(rotate, 100 * Time.fixedDeltaTime);
+            }
+
+            if(enableWASD) {
+                Vector3 deltaPosition = new Vector3(
+                    moveVertical,
+                    moveUp,
+                    moveHorizontal);
+                deltaPosition *= 0.05f;
+                rb.velocity = deltaPosition / Time.fixedDeltaTime;
+            }
+           
+        }
+    }
+}
