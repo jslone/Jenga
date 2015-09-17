68a4071a59553589747792d26d0bacf15ca5a61c
Jacob Slone <jslone1993@gmail.com>
2015-09-17 18:58:27 -0400

    adding sound effects

---
+++ Assets/Scripts/Block/ImpactSound.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Collider))]
+public class ImpactSound : MonoBehaviour
+{
+    public AudioSource impactSound;
+    public AudioSource sustainedContactSound;
+    public float scaleVolume = 0.1f;
+
+    private Dictionary<Collider, float> colliderVolumeContribution = new Dictionary<Collider, float>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        float volume = scaleVolume * col.relativeVelocity.magnitude;
+        impactSound.volume = volume;
+        sustainedContactSound.volume += volume;
+
+        colliderVolumeContribution[col.collider] = volume;
+
+        impactSound.Play();
+
+        sustainedContactSound.time = Random.Range(0, sustainedContactSound.clip.length);
+        sustainedContactSound.Play();
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+        if (!sustainedContactSound.isPlaying)
+        {
+            sustainedContactSound.Play();
+            sustainedContactSound.time = Random.Range(0.0f, sustainedContactSound.clip.length * 0.999f);
+        }
+
+        float newVolume = scaleVolume * col.relativeVelocity.magnitude;
+        float oldVolume;
+
+        colliderVolumeContribution.TryGetValue(col.collider, out oldVolume);
+
+        sustainedContactSound.volume += newVolume - oldVolume;
+
+        colliderVolumeContribution[col.collider] = newVolume;
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        sustainedContactSound.Stop();
+
+        sustainedContactSound.volume -= colliderVolumeContribution[col.collider];
+
+        colliderVolumeContribution[col.collider] = 0;
+    }
+
+}
