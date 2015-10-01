00e0582ee740fc5691310375f9d16faa9c0b1832
Jacob Slone <jslone1993@gmail.com>
2015-09-30 20:23:46 -0400

    fixing hand orientation

---
+++ Assets/FollowBelow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowBelow : MonoBehaviour
+{
+    public Transform toFollow;
+    public Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = toFollow.TransformPoint(offset);
+
+        transform.position = pos;
+        transform.forward = toFollow.forward;
+    }
+}
---
+++ Assets/Scripts/Utilities/LockDirection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockDirection : MonoBehaviour
+{
+    public bool up;
+    public bool right;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.up = up ? Vector3.up : transform.parent.up;
+        transform.right = right ? Vector3.right : transform.parent.right;
+    }
+}
