bd2467a923d70bb74f0cc2e35754aae671c580ce
Jacob Slone <jslone1993@gmail.com>
2015-10-07 15:01:53 -0400

    hopefully fixing git issues

---
+++ Assets/Scripts/Input/GestureControls/ColorByConfidence.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class ColorByConfidence : MonoBehaviour
+{
+    public RiggedHand hand;
+    public Color BadColor;
+    private Color GoodColor;
+    private Renderer rend;
+    
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        GoodColor = rend.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.material.color = Color.Lerp(BadColor, GoodColor, hand.GetLeapHand().Confidence);
+    }
+}
