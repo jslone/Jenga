8474e05a21723dfeb57260d1ca2d864fbff6de1a
Jacob Slone <jslone1993@gmail.com>
2015-10-08 19:13:19 -0400

    focus min height and camera focuses on reset after lose

--- Assets/Scripts/Input/FollowBlocks.cs
+++ Assets/Scripts/Input/FollowBlocks.cs
@@ -7,8 +7,12 @@ public class FollowBlocks : Singleton<FollowBlocks>
     public float MaxHeldBias = 10.0f;
     public float tunedDistance = 4.5f;
     public float handBias = 10.0f;
+    public float minHeight = 1.2f;
+    public GameObject Reset;
+    public GameObject Exit;
     GameObject[] blocks;
     public Transform handTracker;
+    GameOver go;
 
     void Awake()
     {
@@ -19,32 +23,41 @@ public class FollowBlocks : Singleton<FollowBlocks>
     void Start()
     {
         blocks = GameObject.FindGameObjectsWithTag("Block");
+        go = GameObject.FindObjectOfType<GameOver>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
         Vector3 com = Vector3.zero;
-        foreach (GameObject block in blocks)
+        if (go.enabled)
         {
-            com += block.transform.position;
+            foreach (GameObject block in blocks)
+            {
+                com += block.transform.position;
+            }
+
+            float bias = (Mathf.Min(CurrentHeldBias, MaxHeldBias) - 1.0f) * (tunedDistance / CameraControls.Instance.Distance);
+
+            foreach (DragableBlock block in DragableBlock.held)
+            {
+                com += bias * block.transform.position;
+            }
+
+            if (HandControls.Instance.Active)
+            {
+                com += handBias * handTracker.localPosition;
+            }
+
+            com /= blocks.Length;
+        }
+        else
+        {
+            com = 0.5f * Reset.transform.position + 0.5f * Exit.transform.position;
         }
 
-        float bias = (Mathf.Min(CurrentHeldBias, MaxHeldBias) - 1.0f) * (tunedDistance / CameraControls.Instance.Distance);
-
-        foreach(DragableBlock block in DragableBlock.held)
-        {
-            com += bias * block.transform.position;
-        }
-
-        if(HandControls.Instance.Active)
-        {
-            com += handBias * handTracker.localPosition;
-        }
-
-        com /= blocks.Length;
-        transform.position = Vector3.Lerp(transform.position, com, Time.deltaTime);
+        com.y = Mathf.Max(minHeight, com.y);
+        transform.position = Vector3.Lerp(transform.position, com, (go.enabled ? 1.0f : 0.5f) * Time.deltaTime);
         
     }
 }
